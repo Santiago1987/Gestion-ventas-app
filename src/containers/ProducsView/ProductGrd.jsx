567c978e8b71266defa1dd47d930bfb50af9342e
SRA <santiagora1987@gmail.com>
2020-09-08 21:15:53 -0300

    cambios en product view

---
+++ src/containers/ProducsView/ProductGrd.jsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { loadProducts, selectProd, addSalesProduct } from "../../actions";
+
+import products from "../../data";
+import Table from "../../components/Table/Table";
+
+const ProductGrd = () => {
+  const [Loading, setLoading] = useState(true);
+
+  // Reducers
+  const Products = useSelector((state) => state.Products);
+  const selectedProd = useSelector((state) => state.selectedProd);
+  const Search = useSelector((state) => state.Search);
+
+  const dispatch = useDispatch();
+  const dolar = 70;
+
+  useEffect(() => {
+    let data = [...products];
+
+    //fetching data from server
+    setTimeout(() => {
+      let newData = data.map((d) => ({
+        ...d,
+        precioFinal: Math.round(d.precio * dolar, 2),
+        precio1: Math.round((d.precio * dolar) / 0.9, 2),
+        precio2: Math.round(d.precio * dolar * 1.3, 2),
+      }));
+
+      dispatch(loadProducts(newData));
+      setLoading(false);
+    }, 3000);
+  }, [Products, Search]);
+
+  // Row selection
+  const handleRowSelect = (id, table) => {
+    dispatch(selectProd({ id: id, table: table }));
+  };
+
+  const handleOnDobleClick = (id) => {
+    dispatch(addSalesProduct(Products.find((p) => p.id === id)));
+  };
+
+  const handleOnChange = (id) => null;
+
+  // Columns titles
+  const titles = {
+    id: "id",
+    numero: "#",
+    stock: "Stock",
+    descr: "Descripcion",
+    PUS: "Precio $US",
+    PF: "Precio Final",
+    P1: "Precio 1",
+    P2: "Precio 2",
+  };
+
+  // Initializtions
+  let items = [];
+  let rowN = 1;
+  let filteredProd = [];
+
+  // Search funcionality
+  if (Search === "") {
+    filteredProd = Products;
+  } else {
+    filteredProd = Products.filter((prod) => {
+      return prod.descripcion.toLowerCase().includes(Search.toLowerCase());
+    });
+  }
+
+  // Products
+  if (filteredProd.length > 0) {
+    items = filteredProd.map((prod) => ({
+      id: prod.id,
+      number: rowN++,
+      stock: prod.stock,
+      descr: prod.descripcion,
+      PUS: prod.precio,
+      PF: prod.precioFinal,
+      P1: prod.precio1,
+      P2: prod.precio2,
+    }));
+  }
+
+  // Render
+  let content = (
+    <>
+      <div className="row container shadow bg-white rounded py-2">
+        <Table type="PRODUCTS" titles={titles} />
+        <p>Loading products... </p>
+      </div>
+    </>
+  );
+
+  if (!Loading && items.length > 0) {
+    content = (
+      <>
+        <div className="container shadow bg-white rounded py-2">
+          <Table type="PRODUCTS" titles={titles} />
+          <div className="shadow bg-white rounded prodTable">
+            <Table
+              type="PRODUCTS"
+              items={items}
+              handleRowSelect={handleRowSelect}
+              selectedProd={selectedProd}
+              handleOnDobleClick={handleOnDobleClick}
+              handleOnChange={handleOnChange}
+            />
+          </div>
+        </div>
+      </>
+    );
+  } else if (!Loading && !Products && Products.length === 0) {
+    console.log("error feching products data from server");
+    content = <p>Error feching products data from server</p>;
+  }
+
+  return content;
+};
+
+export default React.memo(ProductGrd);
--- src/containers/ProducsView/ProductsList.jsx
+++ src/containers/ProducsView/ProductsList.jsx
@@ -1,59 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Modal from "react-modal";
-import { loadProducts, selectProd, addSalesProduct } from "../../actions";
+import { loadProducts, selectProd } from "../../actions";
 
-import Table from "../../components/Table/Table";
-import products from "../../data";
 import Input from "../../components/Input/Input";
 import Btn from "../../components/Button/Button";
 import ArticleModal from "../../components/Modal/ArticleModal";
+import ProductGrd from "./ProductGrd";
 
 const ProductsView = () => {
-  const [Loading, setLoading] = useState(false);
   const [prodModalOpen, setprodModalOpen] = useState(false);
 
   // modal props
   const [type, setType] = useState("");
   const [gridType, setGridType] = useState("");
 
-  // Reducers
-  const Products = useSelector((state) => state.Products);
-  const Search = useSelector((state) => state.Search);
-  const selectedProd = useSelector((state) => state.selectedProd);
-
   const dispatch = useDispatch();
-  const dolar = 70;
-
-  useEffect(() => {
-    setLoading(true);
-
-    let data = [...products];
-
-    //fetching data from server
-    setTimeout(() => {
-      let newData = data.map((d) => ({
-        ...d,
-        precioFinal: Math.round(d.precio * dolar, 2),
-        precio1: Math.round((d.precio * dolar) / 0.9, 2),
-        precio2: Math.round(d.precio * dolar * 1.3, 2),
-      }));
-
-      dispatch(loadProducts(newData));
-      setLoading(false);
-    }, 3000);
-  }, []);
-
-  // Row selection
-  const handleRowSelect = (id, table) => {
-    dispatch(selectProd({ id: id, table: table }));
-  };
-
-  const handleOnDobleClick = (id) => {
-    dispatch(addSalesProduct(Products.find((p) => p.id === id)));
-  };
-
-  const handleOnChange = (id) => null;
 
   const handleOnClickBtn = (typ, gtyp) => {
     // en caso de no seleccion y que se use un boton mandar un warning
@@ -71,45 +33,6 @@ const ProductsView = () => {
     if (type === "new") dispatch(loadProducts([producto]));
   };
 
-  // Columns titles
-  const titles = {
-    id: "id",
-    numero: "#",
-    stock: "Stock",
-    descr: "Descripcion",
-    PUS: "Precio $US",
-    PF: "Precio Final",
-    P1: "Precio 1",
-    P2: "Precio 2",
-  };
-
-  // Initializtions
-  let items = [];
-  let rowN = 1;
-  let filteredProd = [];
-
-  // Search funcionality
-  if (Search === "") {
-    filteredProd = Products;
-  } else {
-    filteredProd = Products.filter((prod) => {
-      return prod.descripcion.toLowerCase().includes(Search.toLowerCase());
-    });
-  }
-
-  // Products
-  if (filteredProd.length > 0) {
-    items = filteredProd.map((prod) => ({
-      id: prod.id,
-      number: rowN++,
-      stock: prod.stock,
-      descr: prod.descripcion,
-      PUS: prod.precio,
-      PF: prod.precioFinal,
-      P1: prod.precio1,
-      P2: prod.precio2,
-    }));
-  }
   const customStyles = {
     content: {
       top: "50%",
@@ -122,79 +45,52 @@ const ProductsView = () => {
   };
 
   // Render
-  let content = <p>Loading products... </p>;
+  let content = (
+    <>
+      <Input />
+      <ProductGrd />
+      <div className="shadow bg-white rounded m-3 d-flex flex-row">
+        <Btn
+          type="new"
+          gridType="PRODUCTS"
+          title="Nuevo"
+          classes="btn btn-dark m-2"
+          onclick={handleOnClickBtn}
+          styles={{ width: "100px" }}
+        />
+        <Btn
+          gridType="PRODUCTS"
+          type="update"
+          title="Modificar"
+          classes="btn btn-dark m-2"
+          onclick={handleOnClickBtn}
+          styles={{ width: "100px" }}
+        />
+        <Btn
+          gridType="PRODUCTS"
+          type="delete"
+          title="Borrar"
+          classes="btn btn-dark m-2"
+          onclick={handleOnClickBtn}
+          styles={{ width: "100px" }}
+        />
+      </div>
+      <Modal
+        isOpen={prodModalOpen}
+        onRequestClose={() => setprodModalOpen(false)}
+        shouldCloseOnOverlayClick={false}
+        closeTimeoutMS={200}
+        style={customStyles}
+      >
+        <ArticleModal
+          btnType={type}
+          gridType={gridType}
+          handleOnSubmit={handleOnSubmit}
+        />
+      </Modal>
+    </>
+  );
 
-  if (items.length === 0) {
-    content = (
-      <>
-        <Input />
-        <div className="row container shadow bg-white rounded py-2">
-          <Table type="PRODUCTS" titles={titles} />
-        </div>
-      </>
-    );
-  } else if (!Loading && items.length > 0) {
-    content = (
-      <>
-        <Input />
-        <div className="container shadow bg-white rounded py-2">
-          <Table type="PRODUCTS" titles={titles} />
-          <div className="shadow bg-white rounded prodTable">
-            <Table
-              type="PRODUCTS"
-              items={items}
-              handleRowSelect={handleRowSelect}
-              selectedProd={selectedProd}
-              handleOnDobleClick={handleOnDobleClick}
-              handleOnChange={handleOnChange}
-            />
-          </div>
-        </div>
-        <div className="shadow bg-white rounded m-3 d-flex flex-row">
-          <Btn
-            type="new"
-            gridType="PRODUCTS"
-            title="Nuevo"
-            classes="btn btn-dark m-2"
-            onclick={handleOnClickBtn}
-            styles={{ width: "100px" }}
-          />
-          <Btn
-            gridType="PRODUCTS"
-            type="update"
-            title="Modificar"
-            classes="btn btn-dark m-2"
-            onclick={handleOnClickBtn}
-            styles={{ width: "100px" }}
-          />
-          <Btn
-            gridType="PRODUCTS"
-            type="delete"
-            title="Borrar"
-            classes="btn btn-dark m-2"
-            onclick={handleOnClickBtn}
-            styles={{ width: "100px" }}
-          />
-        </div>
-        <Modal
-          isOpen={prodModalOpen}
-          onRequestClose={() => setprodModalOpen(false)}
-          shouldCloseOnOverlayClick={false}
-          closeTimeoutMS={200}
-          style={customStyles}
-        >
-          <ArticleModal
-            btnType={type}
-            gridType={gridType}
-            handleOnSubmit={handleOnSubmit}
-          />
-        </Modal>
-      </>
-    );
-  } else if (!Loading && !Products && Products.length === 0) {
-    console.log("error feching products data from server");
-    content = <p>Error feching products data from server</p>;
-  }
   return content;
 };
 
