9f9ab3eefa3b16abf1e4461c5a48f42d363af199
Santiago <santiagora1987@gmail.com>
2021-10-17 16:02:53 -0300

    new hooks for the grid products

--- src/containers/ProducsView/ProductGrd.jsx
+++ src/containers/ProducsView/ProductGrd.jsx
@@ -1,34 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  loadProducts,
-  selectProd,
-  addSalesProduct,
-  updateProduct,
-  iniProducts,
-} from "../../actions";
-import axios from "axios";
-
+import { loadProducts, iniProducts } from "../../actions";
 import Table from "../../components/Table/Table";
 import { useHttp } from "../hooks/http";
+import useProductGRDFunctions from "../hooks/productGRDFunctions";
 
 const ProductGrd = () => {
   // Reducers
   const Products = useSelector((state) => state.Products);
-  //const selectedProd = useSelector((state) => state.selectedProd);
   const Search = useSelector((state) => state.Search);
-  const {
-    REACT_APP_BACKEND_URL,
-    REACT_APP_ARTICLE_LIST_URL,
-    REACT_APP_SAVE_STKMOV_URL,
-  } = process.env;
+
+  const [
+    getListOfProducts,
+    handleRowSelect,
+    handleOnDobleClick,
+    handleKeyPress,
+    handleOnChange,
+  ] = useProductGRDFunctions;
+
+  const { REACT_APP_BACKEND_URL, REACT_APP_ARTICLE_LIST_URL } = process.env;
 
   const dispatch = useDispatch();
-  const dolar = 70;
-  const porcLocal = 10;
-  const porcML = 25;
 
-  //const [Loading, setLoading] = useState(true);
   const [Loading, getData] = useHttp(
     `${REACT_APP_BACKEND_URL}${REACT_APP_ARTICLE_LIST_URL}`,
     [],
@@ -40,91 +33,10 @@ const ProductGrd = () => {
     dispatch(iniProducts());
     if (getData !== null) {
       let { data } = getData;
-      let Artlist = data.map((d) => {
-        let { _id, descripcion, prDolar, stock, ingreso } = d;
-
-        return {
-          id: _id,
-          descripcion,
-          prDolar,
-          stock,
-          ingreso,
-          prPesos: Math.round((prDolar * dolar + Number.EPSILON) * 100) / 100,
-          prLocal:
-            Math.round(
-              ((prDolar * dolar) / ((100 - porcLocal) / 100) + Number.EPSILON) *
-                100
-            ) / 100,
-          prML:
-            Math.round(
-              (prDolar * dolar * (porcML / 100 + 1) + Number.EPSILON) * 100
-            ) / 100,
-        };
-      });
+      getListOfProducts(data);
       dispatch(loadProducts(Artlist));
     }
   }, [getData]);
-
-  // Row selection
-  const handleRowSelect = (id, table) => {
-    dispatch(selectProd({ id: id, table: table }));
-  };
-
-  const handleOnDobleClick = (id, column) => {
-    let precio = 0;
-    let type = "L";
-    let articulo = Products.find((p) => p.id === id);
-
-    if (column === "PML") {
-      precio = articulo.prML;
-      type = "ML";
-    } else precio = articulo.prLocal;
-
-    dispatch(
-      addSalesProduct({
-        id: articulo.id,
-        descripcion: articulo.descripcion,
-        precio,
-        type,
-      })
-    );
-  };
-
-  const handleKeyPress = async (e) => {
-    let { key } = e;
-    let { value, id } = e.target;
-    let response = null;
-
-    if (key === "Enter") {
-      await axios
-        .post(`${REACT_APP_BACKEND_URL}${REACT_APP_SAVE_STKMOV_URL}`, {
-          id,
-          cant: value,
-        })
-        .then((res) => {
-          response = res.data;
-        })
-        .catch((err) => {
-          console.log("error", err);
-        });
-      let { _id, descripcion, ingreso, prDolar, stock } = response;
-      if (_id === 0) return; //errrrorrr
-      dispatch(
-        updateProduct({ id: _id, descripcion, ingreso, prDolar, stock })
-      );
-    }
-  };
-
-  const handleOnChange = (e) => {
-    let { value, id } = e.target;
-    let regex = new RegExp("^([0-9])*$");
-
-    if (value.length > 5 || !regex.test(value)) return;
-
-    let articulo = Products.find((pr) => pr.id === id);
-    articulo.ingreso = value;
-    dispatch(updateProduct(articulo));
-  };
 
   // Columns titles
   const titles = {
@@ -185,7 +97,6 @@ const ProductGrd = () => {
               type="PRODUCTS"
               items={items}
               handleRowSelect={handleRowSelect}
-              //selectedProd={selectedProd}
               handleOnDobleClick={handleOnDobleClick}
               handleKeyPress={handleKeyPress}
               handleOnChange={handleOnChange}
---
+++ src/containers/hooks/productGRDFunctions.js
@@ -0,0 +1,124 @@
+import { useDispatch, useSelector } from "react-redux";
+import { addSalesProduct, loadProducts, selectProd } from "../../actions";
+
+const useProductGRDFunctions = () => {
+  // List of products
+  const productList = useSelector((state) => state.Products);
+
+  const dispatch = useDispatch();
+
+  // Dolar price
+  const dolar = 70;
+
+  // Percentage of local increase
+  const porcLocal = 10;
+
+  // Percentage of Mercado Libre increase
+  const porcML = 25;
+
+  // enviroment variables: URL for backoffice
+  const { REACT_APP_BACKEND_URL, REACT_APP_SAVE_STKMOV_URL } = process.env;
+
+  // Get list of products from database
+  const getListOfProducts = (data) => {
+    let artlist = data.map((art) => {
+      let { _id, description, prDolar, stock, arraivals } = art;
+
+      return {
+        id: _id,
+        description,
+        prDolar,
+        stock,
+        arraivals,
+        prPesos: Math.round((prDolar * dolar + Number.EPSILON) * 100) / 100,
+        prLocal:
+          Math.round(
+            ((prDolar * dolar) / ((100 - porcLocal) / 100) + Number.EPSILON) *
+              100
+          ) / 100,
+        prMl:
+          Math.round(
+            (prDolar * dolar * (porcML / 100 + 1) + Number.EPSILON) * 100
+          ) / 100,
+      };
+    });
+
+    dispatch(loadProducts(artlist));
+  };
+
+  // Row selection
+  const handleRowSelect = (id, table) => {
+    dispatch(selectProd(id, table));
+  };
+
+  // functin to select an article for sale
+  const handleOnDobleClick = (id, column) => {
+    let precio = 0;
+    let type = "L";
+    let product = productList.find((p) => p.id === id);
+
+    if (product === undefined) return;
+
+    if (column === "PML") {
+      precio = product.prMl;
+      type = "ML";
+    } else precio = prduct.prLocal;
+
+    dispatch(
+      addSalesProduct({
+        id: product.id,
+        descripcion: product.descripcion,
+        precio,
+        type,
+      })
+    );
+  };
+
+  // enter arraivals for products
+  const handleKeyPress = async (e) => {
+    let { key } = e;
+    let { value, id } = e.target;
+    let response = null;
+
+    if (key === "Enter") {
+      await axios
+        .post(`${REACT_APP_BACKEND_URL}${REACT_APP_SAVE_STKMOV_URL}`, {
+          id,
+          cant: value,
+        })
+        .then((res) => {
+          response = res.data;
+        })
+        .catch((err) => {
+          console.log("error", err);
+        });
+      let { _id, descripcion, ingreso, prDolar, stock } = response;
+      if (_id === 0) return; //errrrorrr
+      dispatch(
+        updateProduct({ id: _id, descripcion, ingreso, prDolar, stock })
+      );
+    }
+  };
+
+  // manage the arraivals inputs
+  const handleOnChange = (e) => {
+    let { value, id } = e.target;
+    let regex = new RegExp("^([0-9])*$");
+
+    if (value.length > 5 || !regex.test(value)) return;
+
+    let articulo = Products.find((pr) => pr.id === id);
+    articulo.ingreso = value;
+    dispatch(updateProduct(articulo));
+  };
+
+  return [
+    getListOfProducts,
+    handleRowSelect,
+    handleOnDobleClick,
+    handleKeyPress,
+    handleOnChange,
+  ];
+};
+
+export default useProductGRDFunctions;
